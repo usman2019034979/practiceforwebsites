1de75d2829374e28c72d7dded8fe2397a4fd80a9
usman2019034979 <96358214+usman2019034979@users.noreply.github.com>
2021-12-19 05:00:49 +0500

    practiceforweb

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+	//-------------------------------
+	// Mobile Nav Menu
+	//-------------------------------
+	if($("#agony-meter-nav").length) {
+		$('#agony-meter-nav').hcOffcanvasNav({
+			maxWidth: 1200
+		});
+	}
+	
+	//accordion 
+
+});
